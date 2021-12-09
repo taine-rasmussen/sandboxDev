865512fa79e4756a086688e51e6ab9294a883b56
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-09 16:27:43 +1300

    super good commint message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './App.css';
 
+// Components
+import Landing from './Components/Landing'
+
+
 function App() {
 
   return (
     <div className="App">
-   
+
+
+      shit do be working
     </div>
   );
 }
---
+++ src/Components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Landing.css'
+
+
+const Landing = () => {
+   return (
+      <div>
+         
+      </div>
+   )
+}
+
+export default Landing
