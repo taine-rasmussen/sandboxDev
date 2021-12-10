722e493dc0ee1d359a24ba39ca6742273be2d09d
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-10 14:32:13 +1300

    added logincss file / basic positioning to login header

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Login.css'
 
 const Login = () => {
    return (
-      <div>
-         <Link to='/'>
-            <button>Home</button>
-         </Link>
+      <div className="login-container">
+         <div className="login-header-container">
+            <h1>Example log in page</h1>
+            <Link to='/'>
+               <button>Home</button>
+            </Link>
+         </div>
+
       </div>
    )
 }
