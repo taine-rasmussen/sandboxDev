ce8868b4814386a78386d9fc626892a32bb93b05
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-14 17:16:33 +1300

    refactored landing page

--- src/Components/Hero/Menu.jsx
+++ src/Components/Hero/Menu.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Heroleft.css'
+import { Link } from 'react-router-dom'
 
 
 
 const Menu = () => {
    return (
       <div className="menu-container">
-         <li>Home</li>
+         <Link to='/'>
+            <li>Home</li>
+         </Link>
          <li>Biography</li>
          <li> Galleries</li>
          <li> Trips</li>
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -3,22 +3,26 @@ import '../Styles/Landing.css';
 import { Link } from 'react-router-dom'
 
 const Landing = () => {
+
+
+   const projects = ['login', 'hero']
+
+
    return (
       <div className="landing-container">
          <div className="landing-header">
-            <h1>Welcome to the Sandbox</h1>
+            <h1>Welcome to the <span>Sandbox</span></h1>
          </div>
          <div className="landing-links-container">
-            <div className="landing-link">
-               <Link to='login'>
-                  <button>Log in page</button>
-               </Link>
+           {projects.map((proj, index) => {
+              return(
+               <div className="landing-link" key={index}>
+                  <Link to={proj}>
+                     <button>{proj.charAt(0).toUpperCase() + proj.slice(1)}</button>
+                  </Link>
             </div>
-            <div className="landing-link">
-               <Link to='hero'>
-                  <button>Hero example</button>
-               </Link>
-            </div>
+              )
+           })}
          </div>
       </div>
    )
