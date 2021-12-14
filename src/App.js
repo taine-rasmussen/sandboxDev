3ad04f6609467fe9a0f1cbfe38fcd80439df093e
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-14 13:57:13 +1300

    created files for hero example build

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes  } from 'react-router-dom'
 // Components
 import Landing from './Components/Landing'
 import Login from './Components/Login/Login'
+import Hero from './Components/Hero/Hero'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Landing />}/>
         <Route exact path='/login' element={<Login />}/>
+        <Route exact path='/hero' element={<Hero />}/>
       </Routes>
     </div>
   );
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+   return (
+      <div className="hero-container">
+
+      </div>
+   )
+}
+
+export default Hero
