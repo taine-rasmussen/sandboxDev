5b8ecc77c8f5b30891b59ed21735dbf43619808a
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-16 21:17:59 +1300

    I have no idea whats in here

--- src/Components/Hero/Social.jsx
+++ src/Components/Hero/Social.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import './Heroleft.css'
 
 const Social = () => {
+
+
    return (
       <div className="social-container">
-         social links will go here
+         <div className="social-link-container">
+            <div className="social-link">
+               <i className="fa fa-facebook"></i>
+            </div>
+            <div className="social-link">
+               <i className="fa fa-twitter"></i>
+            </div>
+            <div className="social-link">
+               <i className="fa fa-youtube"></i>
+            </div>
+            <div className="social-link">
+               <i className="fa fa-instagram"></i>
+            </div>   
+         </div>
+         <div className="social-separator"></div>
       </div>
    )
 }
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -13,15 +13,15 @@ const Landing = () => {
          <div className="landing-header">
             <h1>Welcome to the <span>Sandbox</span></h1>
          </div>
-         <div className="landing-links-container">
-           {projects.map((proj, index) => {
-              return(
-               <div className="landing-link" key={index}>
-                  <Link to={proj}>
-                     <button>{proj.charAt(0).toUpperCase() + proj.slice(1)}</button>
-                  </Link>
-            </div>
-              )
+            <div className="landing-links-container">
+            {projects.map((projName, index) => {
+               return(
+                  <div className="landing-link" key={index}>
+                    <Link to={projName}>
+                        <button>{projName.charAt(0).toUpperCase() + projName.slice(1)}</button>
+                     </Link>
+                  </div>
+               )
            })}
          </div>
       </div>
