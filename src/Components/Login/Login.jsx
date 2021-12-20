e6dba88abd73dfb2b9ea105bc074ab8dc82d5c8b
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-20 20:53:03 +1300

    set up and styled user input

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
    return (
       <div className="login-container">
          <div className="login-body-container">
-
+            <User />
          </div>
       </div>
    )
--- src/Components/Login/User.jsx
+++ src/Components/Login/User.jsx
@@ -5,7 +5,23 @@ import './User.css'
 const User = () => {
    return (
       <div className="user-container">
-        
+        <div className="user-header">
+            <h1>Welcome Back!</h1>
+        </div>
+        <div className="user-form">
+            <div className="user-input">
+               <label>EMAIL</label>
+               <input 
+                  type="email"
+               />
+            </div>
+            <div className="user-input">
+               <label>PASSWORD</label>
+               <input 
+                  type="password"
+               />
+            </div>
+        </div>
       </div>
    )
 }
