80811dd1b24fbc8b2b52774dc7acd2ec3abead84
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-29 23:51:08 +1300

    win condition finished and working

--- src/Components/Memory/GameInput.jsx
+++ src/Components/Memory/GameInput.jsx
@@ -2,33 +2,43 @@ import React, {useState} from 'react'
 
 const GameInput = ({pattern, winningGrid}) => {
 
-
    const [gameHeader, setGameHeader] = useState('')
 
    let gameTracker = [[1], [1], [1], [1], [1], [1], [1], [1], [1]]
 
+   // Function used on player click checking win conditions & updating game header on win or loss
+   const checkForWin = () => {
+      let filledCells = null
+      let playerFilledCells = 0
+      // Gets num of cells filled from generated pattern
+      winningGrid.forEach((cell) => {
+         if(cell == 0){
+            return filledCells += 1
+         }
+      })
+      // Gets num of cells filled by player
+      gameTracker.forEach((cell) => {
+         if(cell == 0){
+            return playerFilledCells += 1
+         }
+      })
+      // Checks if playerFilledCells and generated pattern are the same and updates gameHeader
+      if(filledCells == playerFilledCells){
+         setGameHeader('You Won!')
+      }
+   }
 
    // Checks if cell was part of pattern and changes bg if true else triggers gameover header to appear
-   const handleClick = (id) => { 
-      
-     // Handles changing bg colour of cell and triggering gameover on wrong cell click
+   const handleClick = (id) => {  
      if(document.getElementById(`${id}`) && pattern[id].pattern == 0){
       document.getElementById(`${id}`).style.background = '#000'
       gameTracker[id] = [0]
-      console.log(gameTracker)
+      checkForWin()
      }
      else {
-        setGameHeader('Game over')
+        setGameHeader('Game over!')
      }
    }
-
-   // Checks if winningGrid === gameTracker triggering win notification
-   const checkForWin = () => {
-
-   }
-   checkForWin()
-
-   // console.log(winningGrid)
 
    return ( 
       <>
