ac7cc5ad8f937204b18ac099433514fa5aef137a
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-12-29 15:40:40 +1300

    refactored sort data

--- src/Components/Memory/GameInput.jsx
+++ src/Components/Memory/GameInput.jsx
@@ -1,43 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-const GameInput = ({gridPattern, emptyGrid, setGridPattern}) => {
+const GameInput = ({pattern}) => {
 
-   const [bgColour, setBgColour] = useState({backgroundColor: '#fff'})
-   const [pattern, setPattern] = useState([])
 
    const handleClick = (id) => { 
    }
 
 
-   // Takes generated pattern and addeds it to nested array with id's attached
-   const sortData = () => {
-      gridPattern.forEach((cell, index) => {
-         pattern.push({id: index, pattern: cell})
-      })
-   }
-   // Calls sort Data function on page load
-   useEffect(() => {
-      sortData()
-   }, []);
-   
-
-   return (
-      
-      <div className="memory-game-container">
-         {emptyGrid.map((cell, index) => {
-            return(
-               <div 
-                  className="memory-game-cell"
-                  key={index}
-                  style={bgColour}
-                  onClick={() => handleClick(index)}
-               >   
-               </div>
-            )
-         })}
-
-
-      </div>
+   return ( 
+         <div className="memory-game-container">
+            {pattern.map((cell, index) => {
+               console.log(cell.style)
+               return(
+                  <div 
+                     className="memory-game-cell"
+                     key={index}
+                     style={cell.style}
+                     onClick={() => handleClick(index)}
+                  >   
+                  </div>
+               )
+            })}
+         </div>
    )
 }
 export default GameInput
--- src/Components/Memory/Gameboard.jsx
+++ src/Components/Memory/Gameboard.jsx
@@ -9,17 +9,28 @@ const Gameboard = () => {
    const [changeGrid, setChangeGrid] = useState(false)
    const [emptyGrid, setEmptyGrid] = useState([[], [], [], [], [], [], [], [], []])
    const [gridPattern, setGridPattern] = useState([[Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)]])
+   const [pattern, setPattern] = useState([])
+
+
+    // Takes generated pattern and addeds it to nested array with id's attached
+   const sortData = () => {
+      gridPattern.forEach((cell, index) => {  
+        pattern.push({id: index, pattern: cell, style: {backgroundColor: '#fff'}})
+      })
+   }
+
 
 
    // Updates changeView after 5secs changing the component rendered to GameInput - Creates and displays pattern
    const createPattern = () => {
       setChangeGrid(true)
+      sortData()
       setInterval(function () {setChangeView(true)}, 500);
    }
 
    return(
       <>
-      {changeView ? <GameInput gridPattern={gridPattern} emptyGrid={emptyGrid} setGridPattern={setGridPattern}/> : 
+      {changeView ? <GameInput pattern={pattern}/> : 
          <div className='memory-game-container'>
             {changeGrid ? gridPattern.map((cell, index) => {
                return(
