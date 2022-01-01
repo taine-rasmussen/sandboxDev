0f83e3c0a19650becec06baf6113da5b2c210245
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-01-01 21:18:50 +1300

    refgactooasd

--- src/Components/Memory/Gameboard.jsx
+++ src/Components/Memory/Gameboard.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 // Components
 import GameInput from './GameInput'
-
-
 
 // Strech idea: Before player has selected create pattern the grid should randomly change colour of cell backgrounds until create pattern is selected.
 
@@ -18,31 +16,36 @@ const Gameboard = () => {
    const [gridPattern, setGridPattern] = useState([[Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)],[Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)]])
 
 
+   
+   
    // Takes generated pattern and addeds it to nested array with id's attached
    const sortData = () => {
       gridPattern.forEach((cell, index) => {  
-        pattern.push({id: index, pattern: cell, style: {backgroundColor: '#A6CF98'}})
+         pattern.push({id: index, pattern: cell, style: {backgroundColor: '#A6CF98'}})
       })
-
+      
       pattern.forEach((cell) => {
          winningGrid.push([cell.pattern[0]])
       })
    }
-
+   
    // Updates changeView after 5secs changing the component rendered to GameInput - Creates and displays pattern
    const createPattern = () => {
       setChangeGrid(true)
       sortData()
    }
-
+   
    // Resets the game???
    const resetGame = () => {
-         setPattern([])
-         setWinningGrid([])
-         setChangeView(!changeView)
-         setChangeGrid(!changeGrid)
-         setGridPattern([[Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)],[Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)]])
-      }
+      setPattern([])
+      setWinningGrid([])
+      setChangeView(!changeView)
+      setChangeGrid(!changeGrid)
+      setGridPattern([[Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)],[Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)], [Math.floor(Math.random() * 2)]])
+   }
+   
+   const attemptPatternBtn = <button onClick={() => {setChangeView(!changeView)}}>Attempt pattern</button>
+   const createPatternBtn = <button onClick={createPattern}>Create Pattern!</button>
 
    return(
       <>
@@ -63,14 +66,13 @@ const Gameboard = () => {
             : emptyGrid.map((cell, index) => {
                return(
                   <div 
-                     className="memory-game-cell"
+                     className="memory-game-cell empty-hover"
                      key={index}
                      style={{backgroundColor: '#A6CF98'}}
                   ></div> 
                )
             })}
-         {changeGrid ? null : <button onClick={createPattern}>Create Pattern!</button>}             
-         {changeGrid ? <button onClick={() => {setChangeView(!changeView)}}>Attempt pattern</button> : null}
+         {changeGrid ? attemptPatternBtn: createPatternBtn}             
          </div>
       }  
       </>
