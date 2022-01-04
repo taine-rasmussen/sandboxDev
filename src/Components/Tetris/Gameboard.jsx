2ba6013953551f1e69820af00ea8b1c6041c1d2b
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-01-04 23:43:33 +1300

    created grid for gamecells - basic styling

--- src/Components/Tetris/Gameboard.jsx
+++ src/Components/Tetris/Gameboard.jsx
@@ -1,15 +1,25 @@
-import React  from 'react'
+import React, { useState } from 'react'
 import './Gameboard.css'
 
 
 
 const Gameboard = () => {
 
+   const gameCell = <div></div>
+   const [cells, setCells] = useState(Array(200).fill(gameCell))
+
+
+
+
    return (
       <div className="gameboard-container">
          <div className="gameboard-area">
             <div className="gameboard-game">
-
+               {cells.map((cell, index) => {
+                  return(
+                     <div key={index} className="gameboard-cell">{cell}</div>
+                  )
+               })}
             </div>
          </div>
       </div>
