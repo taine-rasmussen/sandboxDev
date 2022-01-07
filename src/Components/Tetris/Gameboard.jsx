25b0f69811a19eef1b2f971fe8eb88a2a04a99cd
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-01-07 22:02:24 +1300

    added header, win state and draw conditions

--- src/Components/Tetris/Gameboard.jsx
+++ src/Components/Tetris/Gameboard.jsx
@@ -6,6 +6,8 @@ const Gameboard = () => {
    const [grid, setGrid] = useState([{id: 0, state: false, value: ''}, {id: 1, state: false, value: ''}, {id: 2, state: false, value: ''}, {id: 3, state: false, value: ''}, {id: 4, state: false, value: ''}, {id: 5, state: false, value: ''}, {id: 6, state: false, value: ''}, {id: 7, state: false, value: ''}, {id: 8, state: false, value: ''},])
 
    const [toggleMove, setToggleMove] = useState(true)
+   const [gameState, setGameState] = useState(false)
+   const [header, setHeader] = useState('Tic-Tac-Toe')
    const [gameTracker, setGameTracker] = useState(
       [[], [], [],
       [], [], [],
@@ -14,52 +16,66 @@ const Gameboard = () => {
    // Checks win conditions across all rows for both players
    const checkForWinRow = () => {
       if (gameTracker[0] === 'X' && gameTracker[1] === 'X' && gameTracker[2] === 'X'){
-         console.log('X won')
+         return setGameState(true), setHeader('X won this round')
       } else if (gameTracker[0] === 'O' && gameTracker[1] === 'O' && gameTracker[2] === 'O') {
-         console.log('O won')
+         return setGameState(true), setHeader('O won this round')
       } 
       else if (gameTracker[3] === 'X' && gameTracker[4] === 'X' && gameTracker[5] === 'X'){
-         console.log('X won')
+        return setGameState(true), setHeader('X won this round')
       } else if (gameTracker[3] === 'O' && gameTracker[4] === 'O' && gameTracker[5] === 'O') {
-         console.log('O won')
+         return setGameState(true), setHeader('O won this round')
       }
       else if (gameTracker[6] === 'X' && gameTracker[7] === 'X' && gameTracker[8] === 'X'){
-         console.log('X won')
+        return setGameState(true), setHeader('X won this round')
       } else if (gameTracker[6] === 'O' && gameTracker[7] === 'O' && gameTracker[8] === 'O') {
-         console.log('O won')
+         return setGameState(true), setHeader('O won this round')
       }
    }
    // Checks win conditions across all cols for both players
    const checkForWinCol = () => {
       if (gameTracker[0] === 'X' && gameTracker[3] === 'X' && gameTracker[6] === 'X'){
-         console.log('X won')
+        return setGameState(true), setHeader('X won this round')
       } else if (gameTracker[0] === 'O' && gameTracker[3] === 'O' && gameTracker[6] === 'O') {
-         console.log('O won')
+         return setGameState(true), setHeader('O won this round')
       } 
       else if (gameTracker[1] === 'X' && gameTracker[4] === 'X' && gameTracker[7] === 'X'){
-         console.log('X won')
+        return setGameState(true), setHeader('X won this round')
       } else if (gameTracker[1] === 'O' && gameTracker[4] === 'O' && gameTracker[7] === 'O') {
-         console.log('O won')
+         return setGameState(true), setHeader('O won this round')
       }
       else if (gameTracker[2] === 'X' && gameTracker[5] === 'X' && gameTracker[8] === 'X'){
-         console.log('X won')
+        return setGameState(true), setHeader('X won this round')
       } else if (gameTracker[2] === 'O' && gameTracker[5] === 'O' && gameTracker[8] === 'O') {
-         console.log('O won')
+         return setGameState(true), setHeader('O won this round')
       }
    }
    // Checks win conditions across all diagonals for both players
    const checkForWinDia = () => {
       if (gameTracker[0] === 'X' && gameTracker[4] === 'X' && gameTracker[8] === 'X'){
-         console.log('X won')
+        return setGameState(true), setHeader('X won this round')
       } else if (gameTracker[0] === 'O' && gameTracker[4] === 'O' && gameTracker[8] === 'O') {
-         console.log('O won')
+         return setGameState(true), setHeader('O won this round')
       } 
       else if (gameTracker[2] === 'X' && gameTracker[4] === 'X' && gameTracker[6] === 'X'){
-         console.log('X won')
+        return setGameState(true), setHeader('X won this round')
       } else if (gameTracker[2] === 'O' && gameTracker[4] === 'O' && gameTracker[6] === 'O') {
-         console.log('O won')
+         return setGameState(true), setHeader('O won this round')
       }
    }
+
+   
+   const checkForDraw = () => {
+      let moveCounter = 0
+         gameTracker.forEach((cell) => {
+            if(cell == 'X' || cell == 'O'){
+               moveCounter += 1
+            }
+         })
+         if(moveCounter === 9 && gameState === false){
+            setHeader(`It's a draw`)
+         } 
+   }
+   
 
 
    // updates player move toggle changing value set "X" or "O" and updates gird with new move
@@ -75,6 +91,7 @@ const Gameboard = () => {
          setGrid([...grid], cell.value = move)
          setToggleMove(!toggleMove)
          gameTracker[id] = move
+         checkForDraw()
          checkForWinRow()
          checkForWinCol()
          checkForWinDia()
@@ -84,6 +101,7 @@ const Gameboard = () => {
 
    return (
       <div className="gameboard-container">
+         <h1>{header}</h1>
          <div className="gameboard-area">
             {grid.map((cell) => (
                <div 
