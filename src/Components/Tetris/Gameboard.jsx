7cef69f5c27b11a8d9f29dfe67a993faabb6f1dd
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-01-07 21:34:05 +1300

    wrote funcs to handle win conditions

--- src/Components/Tetris/Gameboard.jsx
+++ src/Components/Tetris/Gameboard.jsx
@@ -3,9 +3,64 @@ import './Gameboard.css'
 
 const Gameboard = () => {
 
-   const [grid, setGrid] = useState([{id: 1, state: false, value: ''}, {id: 2, state: false, value: ''}, {id: 3, state: false, value: ''}, {id: 4, state: false, value: ''}, {id: 5, state: false, value: ''}, {id: 6, state: false, value: ''}, {id: 7, state: false, value: ''}, {id: 8, state: false, value: ''}, {id: 9, state: false, value: ''},])
+   const [grid, setGrid] = useState([{id: 0, state: false, value: ''}, {id: 1, state: false, value: ''}, {id: 2, state: false, value: ''}, {id: 3, state: false, value: ''}, {id: 4, state: false, value: ''}, {id: 5, state: false, value: ''}, {id: 6, state: false, value: ''}, {id: 7, state: false, value: ''}, {id: 8, state: false, value: ''},])
 
    const [toggleMove, setToggleMove] = useState(true)
+   const [gameTracker, setGameTracker] = useState(
+      [[], [], [],
+      [], [], [],
+      [], [], []],)
+
+   // Checks win conditions across all rows for both players
+   const checkForWinRow = () => {
+      if (gameTracker[0] === 'X' && gameTracker[1] === 'X' && gameTracker[2] === 'X'){
+         console.log('X won')
+      } else if (gameTracker[0] === 'O' && gameTracker[1] === 'O' && gameTracker[2] === 'O') {
+         console.log('O won')
+      } 
+      else if (gameTracker[3] === 'X' && gameTracker[4] === 'X' && gameTracker[5] === 'X'){
+         console.log('X won')
+      } else if (gameTracker[3] === 'O' && gameTracker[4] === 'O' && gameTracker[5] === 'O') {
+         console.log('O won')
+      }
+      else if (gameTracker[6] === 'X' && gameTracker[7] === 'X' && gameTracker[8] === 'X'){
+         console.log('X won')
+      } else if (gameTracker[6] === 'O' && gameTracker[7] === 'O' && gameTracker[8] === 'O') {
+         console.log('O won')
+      }
+   }
+   // Checks win conditions across all cols for both players
+   const checkForWinCol = () => {
+      if (gameTracker[0] === 'X' && gameTracker[3] === 'X' && gameTracker[6] === 'X'){
+         console.log('X won')
+      } else if (gameTracker[0] === 'O' && gameTracker[3] === 'O' && gameTracker[6] === 'O') {
+         console.log('O won')
+      } 
+      else if (gameTracker[1] === 'X' && gameTracker[4] === 'X' && gameTracker[7] === 'X'){
+         console.log('X won')
+      } else if (gameTracker[1] === 'O' && gameTracker[4] === 'O' && gameTracker[7] === 'O') {
+         console.log('O won')
+      }
+      else if (gameTracker[2] === 'X' && gameTracker[5] === 'X' && gameTracker[8] === 'X'){
+         console.log('X won')
+      } else if (gameTracker[2] === 'O' && gameTracker[5] === 'O' && gameTracker[8] === 'O') {
+         console.log('O won')
+      }
+   }
+   // Checks win conditions across all diagonals for both players
+   const checkForWinDia = () => {
+      if (gameTracker[0] === 'X' && gameTracker[4] === 'X' && gameTracker[8] === 'X'){
+         console.log('X won')
+      } else if (gameTracker[0] === 'O' && gameTracker[4] === 'O' && gameTracker[8] === 'O') {
+         console.log('O won')
+      } 
+      else if (gameTracker[2] === 'X' && gameTracker[4] === 'X' && gameTracker[6] === 'X'){
+         console.log('X won')
+      } else if (gameTracker[2] === 'O' && gameTracker[4] === 'O' && gameTracker[6] === 'O') {
+         console.log('O won')
+      }
+   }
+
 
    // updates player move toggle changing value set "X" or "O" and updates gird with new move
    const handleClick = (id) => {
@@ -17,18 +72,15 @@ const Gameboard = () => {
       }
      grid.map((cell) => {
         if(cell.id == id){
-           setGrid([...grid], cell.value = move)
-           setToggleMove(!toggleMove)
+         setGrid([...grid], cell.value = move)
+         setToggleMove(!toggleMove)
+         gameTracker[id] = move
+         checkForWinRow()
+         checkForWinCol()
+         checkForWinDia()
         }
-        return grid
      })
-     return grid
    }
-
-   const checkForWin = () => {
-      
-   }
-
 
    return (
       <div className="gameboard-container">
