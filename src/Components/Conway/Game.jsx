14766351c0132968bef29905418ff76d7059ad99
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-01-07 14:51:48 +1300

    wrote function that generates grid

--- src/Components/Conway/Game.jsx
+++ src/Components/Conway/Game.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React  from 'react'
 import './Conway.css'
 
 const Game = () => {
+
+   const numRows = 30
+   const numCols = 30
+
+   // Creates array gird based of dimensions of above varibles 
+   const createGrid = () => {
+      let grid = [];
+      for (let i = 0; i < numRows; i++) {
+         let row = [];
+      for(let j = 0; j < numCols; j++) {
+         row.push(0)
+         }
+         grid.push(row)
+      }
+      return grid
+   }
+
+   console.log(createGrid())
+
    return (
       <div className="game-container">
-         
+            🛑
       </div>
    )
 }
