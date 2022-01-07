dd99147dd7839f3d6e49e4d15286d2f7c8b526f6
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-01-07 21:05:08 +1300

    proud of this one

--- src/Components/Tetris/Gameboard.jsx
+++ src/Components/Tetris/Gameboard.jsx
@@ -3,8 +3,31 @@ import './Gameboard.css'
 
 const Gameboard = () => {
 
-   const [grid, setGrid] = useState([{id: 1, state: false}, {id: 2, state: false}, {id: 3, state: false}, {id: 4, state: false}, {id: 5, state: false}, {id: 6, state: false}, {id: 7, state: false}, {id: 8, state: false}, {id: 9, state: false},])
+   const [grid, setGrid] = useState([{id: 1, state: false, value: ''}, {id: 2, state: false, value: ''}, {id: 3, state: false, value: ''}, {id: 4, state: false, value: ''}, {id: 5, state: false, value: ''}, {id: 6, state: false, value: ''}, {id: 7, state: false, value: ''}, {id: 8, state: false, value: ''}, {id: 9, state: false, value: ''},])
 
+   const [toggleMove, setToggleMove] = useState(true)
+
+   // updates player move toggle changing value set "X" or "O" and updates gird with new move
+   const handleClick = (id) => {
+      let move = ''
+      if(toggleMove == true){
+         move = 'X'
+      } else {
+         move = 'O'
+      }
+     grid.map((cell) => {
+        if(cell.id == id){
+           setGrid([...grid], cell.value = move)
+           setToggleMove(!toggleMove)
+        }
+        return grid
+     })
+     return grid
+   }
+
+   const checkForWin = () => {
+      
+   }
 
 
    return (
@@ -15,11 +38,16 @@ const Gameboard = () => {
                   key={cell.id}
                   className="gameboard-cell"
                   style={{
+                     display: 'flex',
+                     justifyContent: 'center',
+                     alignItems: 'center',
+                     fontSize: '7rem',
                      border: '1px solid #000',
                      width: 150,
                      height: 150,
                   }}
-               />
+                  onClick={() => handleClick(cell.id)}
+               >{cell.value}</div>
             ))}
          </div>
       </div>
