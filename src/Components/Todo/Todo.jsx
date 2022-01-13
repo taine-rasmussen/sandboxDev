02726705ddb3ede57ec5bccb0cd5d671ae95a350
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-01-13 22:03:23 +1300

    user can edit tasks

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -6,6 +6,8 @@ const Todo = () => {
    
    const [todos, setTodos] = useState([])
    const [input, setInput] = useState('')
+   const [editInput, setEditInput] = useState('')
+   const [editView, setEditView] = useState(false)
 
 
    // udpates state with input text
@@ -20,9 +22,29 @@ const Todo = () => {
       setInput('')
    }
 
+   const editChange = (e) => {
+           e.preventDefault()
+         setEditInput(e.target.value)
+   }
+
+   function handleEdit(id){
+      todos.map((task) => {
+         if(id == task.id){
+            return task.value = editInput
+         }
+      })
+      setEditView(!editView)
+      setEditInput('')
+   }
+
+   // Creats & updates start with new array without including task selected 
    const handleDelete = (id) => {
       const newList = todos.filter((task) => task.id !== id)
       setTodos(newList)
+   }
+
+   function handleEditViewChange () {
+      setEditView(!editView)
    }
    
    return (
@@ -45,7 +67,17 @@ const Todo = () => {
                   return(
                      <div className="todo-single-task" key={task.id}>
                         <h3>{task.value}</h3>
-                        <button onClick={() => handleDelete(task.id)}>Delete</button>
+                        {editView ? <>
+                           <button onClick={() => handleEdit(task.id)}>Add task</button>
+                           <input 
+                              type='text'
+                              onChange={editChange}
+                              value={editInput}
+                           /> 
+                           </> : <> 
+                              <button onClick={handleEditViewChange}>Edit</button> 
+                              <button onClick={() => handleDelete(task.id)}>Delete</button>
+                             </>}
                      </div>
                   )
                })}
